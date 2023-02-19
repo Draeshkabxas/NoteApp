848d27097c7a859457bf542559ed132b50ad2da8
Draeshkabxas <53279075+Draeshkabxas@users.noreply.github.com>
2023-02-19 09:36:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/composenoteapp/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.example.composenoteapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+val Gray = Color(0xFF3B3B3B)
+
+val Black= Color(0xFF252525)
+
+val Bink= Color(0xFFFF9E9E)
+
+val Green= Color(0xFF91F48F)
+
+val Yellow= Color(0xFFFFF599)
+
+val Blue= Color(0xFF9EFFFF)
+
+val Purple= Color(0xFFB69CFF)
+
+val Red= Color(0xFFFF0000)
+
